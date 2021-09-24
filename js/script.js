64a2b72f9c51f1cdc81fe2a4de51da7392a10280
Emma <69841693+xentraz@users.noreply.github.com>
2021-09-24 23:31:27 +0200

    update

--- js/script.js
+++ js/script.js
@@ -4,3 +4,5 @@ import { message } from './components/message.js';
 // Alert message
 message('success', 'This is a success message', '.alert');
 message('warning', 'This is a message', '.alert');
+
+// 
